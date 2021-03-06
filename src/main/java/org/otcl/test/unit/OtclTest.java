7b605d6868c18000a8251e7463f942eef2089729
Franklin <Franklin@franklinton3>
2021-03-06 22:59:53 +0530

    Updated comments

--- src/main/java/org/otcl/test/unit/OtclTest.java
+++ src/main/java/org/otcl/test/unit/OtclTest.java
@@ -15,8 +15,8 @@ public class OtclTest extends AbstractTest {
 	private static OtclEngine otclEngine = OtclEngineImpl.instance;
 	
 	private static enum TEST_METHOD {
-		VALUES_TO_TARGET, 
-		SOURCE_TO_TARGET
+		VALUES_TO_TARGET, // - use this when the OTCL files does not have even a single reference to a source object
+		SOURCE_TO_TARGET  // use this when the OTCL file has references to a source object with or without values.
 	}
 	private static TEST_METHOD testMethod = TEST_METHOD.VALUES_TO_TARGET;
 	private static String pkg = "cpyvalues1";
