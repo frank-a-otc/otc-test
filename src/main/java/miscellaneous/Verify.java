b19768160661698dc87d1e500859cc9ecbfcdf2c
Franklin <frank.j.java@gmail.com>
2022-03-19 23:33:39 +0530

    Refactored

--- src/main/java/miscellaneous/Verify.java
+++ src/main/java/miscellaneous/Verify.java
@@ -7,7 +7,7 @@ import javax.xml.datatype.DatatypeConfigurationException;
 public class Verify {
 
 	public static void main(String[] args) throws DatatypeConfigurationException, IOException {
-
+		System.out.println("Hello");
 
 	}
 
---
+++ src/test/java/org/otcframework/test/unit/OtcTest.java
@@ -0,0 +1,110 @@
+package org.otcframework.test.unit;
+
+import javax.xml.transform.Source;
+
+import org.junit.jupiter.api.Test;
+import org.otcframework.common.OtcConstants;
+import org.otcframework.common.engine.OtcEngine;
+import org.otcframework.common.util.OtcUtils;
+import org.otcframework.core.engine.OtcEngineImpl;
+import org.otcframework.core.engine.compiler.OtclCompiler;
+import org.otcframework.core.engine.compiler.OtclCompilerImpl;
+import org.otcframework.executor.OtcExecutor;
+import org.otcframework.executor.OtcExecutorImpl;
+import org.otcframework.executor.OtcRegistry;
+import org.otcframework.executor.OtcRegistryImpl;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.Comparison;
+import org.xmlunit.diff.ComparisonListener;
+import org.xmlunit.diff.ComparisonResult;
+import org.xmlunit.diff.DOMDifferenceEngine;
+import org.xmlunit.diff.DifferenceEngine;
+
+import com.athena.airlines.dto.AthenaAirlinePassenger;
+import com.kronos.airlines.dto.KronosAirlinePassenger;
+
+
+public class OtcTest {
+
+	private static final OtcEngine otcEngine = OtcEngineImpl.getInstance();
+
+	/** The Constant otcCompiler. */
+	private static final OtclCompiler otclCompiler = OtclCompilerImpl.getInstance();
+	
+	/** The Constant deploymentContainer. */
+	private static final OtcRegistry otcRegistry = OtcRegistryImpl.instance;
+	
+	/** The Constant otcExecutor. */
+	private static final OtcExecutor otcExecutor = OtcExecutorImpl.getInstance();
+
+	private static enum TEST_METHOD {
+		VALUES_TO_TARGET, 
+		SOURCE_TO_TARGET  
+	}
+	
+	// - set 'testMethod' to TEST_METHOD.VALUES_TO_TARGET when the OTC file has 'from: values:' only and does 
+	//    not have even a single reference to a source object
+	// 
+	// set 'testMethod' to  TEST_METHOD.SOURCE_TO_TARGET when the OTC file has references to a source object with
+	//    or without 'from: values:'.
+	
+
+	@Test
+	public void runTest() {
+
+ 		compileAndDeploy();
+ 		
+		String pkg = null; 
+		String otclFile = null;
+		AthenaAirlinePassenger airlinePassenger = null;
+		String otcExpectedResultFile = null;
+		
+//		TEST_METHOD testMethod = TEST_METHOD.VALUES_TO_TARGET;
+		TEST_METHOD testMethod = TEST_METHOD.SOURCE_TO_TARGET;
+		pkg = "overrides";
+		
+ 		if (TEST_METHOD.VALUES_TO_TARGET == testMethod) {
+ 			airlinePassenger = otcExecutor.execute(pkg, AthenaAirlinePassenger.class, null);
+ 			otclFile = OtcUtils.createRegistryId(pkg, null, AthenaAirlinePassenger.class) +
+ 					OtcConstants.OTC_SCRIPT_EXTN; 
+ 			otcExpectedResultFile = OtcUtils.createRegistryId(pkg, null, AthenaAirlinePassenger.class) + ".xml"; 
+		} else if (TEST_METHOD.SOURCE_TO_TARGET == testMethod) {
+ 			KronosAirlinePassenger kronosAirlinePassenger = (KronosAirlinePassenger) TestUtil.loadKronosXml();
+ 			airlinePassenger = otcExecutor.execute(pkg, kronosAirlinePassenger, AthenaAirlinePassenger.class, null);
+ 			otclFile = OtcUtils.createDeploymentId(pkg, kronosAirlinePassenger, AthenaAirlinePassenger.class) +
+ 					OtcConstants.OTC_SCRIPT_EXTN; 
+ 			otcExpectedResultFile = OtcUtils.createDeploymentId(pkg, kronosAirlinePassenger, 
+ 					AthenaAirlinePassenger.class) + ".xml"; 
+		}
+		//-- compare results.
+		System.out.println("\n\nResults for OTCL file: " + otclFile);
+		String result = TestUtil.jaxbObjectToXML(airlinePassenger); 
+		System.out.println("\n" + result);
+		
+//		verify(otcExpectedResultFile, result);
+
+	}
+ 	
+	private void compileAndDeploy() {
+		// -- compile script and generated source code
+		otclCompiler.compile();
+		otclCompiler.compileSourceCode();
+		
+		// -- register the generated .tmd and the executable files.
+		otcRegistry.register();
+	}
+	
+	private void verify(String compareWithFileName, String actualResult) {
+		String expected = TestUtil.getTestCase(compareWithFileName);
+		Source control = Input.fromString(expected).build();
+
+		Source test = Input.fromString(actualResult).build();
+		DifferenceEngine diff = new DOMDifferenceEngine();
+		diff.addDifferenceListener(new ComparisonListener() {
+	        public void comparisonPerformed(Comparison comparison, ComparisonResult outcome) {
+//	            Assert.fail("found a difference: " + comparison);
+	        }
+	    });
+		diff.compare(control, test);
+	}
+}
